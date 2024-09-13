4953660060a4b60d7288f33b184f14a83af35356
DaffaPintu <daffa@pintu.co.id>
2024-09-13 14:29:45 +0700

    FIX

--- src/test/java/SeleniumJayjay.java
+++ src/test/java/SeleniumJayjay.java
@@ -1,23 +1,36 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SeleniumJayjay {
+    public static WebDriver driver;
 
     @Test
     public void helloJayjay() {
         //inisialisasi browser driver
-        WebDriver driver = WebDriverManager.chromedriver().create();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         //navigasi url
         driver.get("https://jayjay.co/courses");
         //get element attribute
-        String text = driver.findElement(By.cssSelector("name-tax")).getText();
+        String text = driver.findElement(By.xpath("//h1")).getText();
         //assertion/pengecekan
         assertEquals("Semua Kursus", text);
+        driver.quit();
 
     }
 }
